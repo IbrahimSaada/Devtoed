046502cb19938598ebcc678865f97003616f4bf4
unknown <nenebattomi@gmail.com>
2025-05-10 11:10:02 +0300

    add entity + req and res for order

---
+++ Devoted.Domain/Sql/Entity/Orders.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Devoted.Domain.Sql.Entity.Base;
+
+namespace Devoted.Domain.Sql.Entity
+{
+    [Table("Orders")]
+    public class Orders : BaseSqlEntity
+    {
+        [Column("productid")]
+        public long ProductId { get; set; }
+
+        [Column("quantity")]
+        public int Quantity { get; set; }
+
+        [Column("total")]
+        public decimal Total { get; set; }
+    }
+}
