851212ff721b4779b4dd1dc8e00198adb318cd48
unknown <nenebattomi@gmail.com>
2025-05-10 10:02:47 +0300

    added entity + important request and repsonse for all crud operations

---
+++ Devoted.Domain/Sql/Entity/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Devoted.Domain.Sql.Entity.Base;
+
+namespace Devoted.Domain.Sql.Entity
+{
+    [Table("Products")]
+    public class Products : BaseSqlEntity
+    {
+        [Column("name")]
+        public string Name { get; set; } = default!;
+
+        [Column("price")]
+        public decimal Price { get; set; }
+    }
+}
